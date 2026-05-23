1d5005979ae5bd81c241f6a9e9f696f97120dc5f
BajiLinux <mr.baji.smart@gmail.com>
2026-05-23 08:46:46 +0700

    Beresin css yang kuning

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -109,7 +109,7 @@ const LoginPage = () => {
         <div className="mt-8">
           <button
             type="button"
-            className="w-full flex items-center justify-center py-2.5 px-4 border border-gray-300 rounded-full shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition duration-150 ease-in-out max-w-[240px] mx-auto"
+            className="w-full flex items-center justify-center py-2.5 px-4 border border-gray-300 rounded-full shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition duration-150 ease-in-out max-w-60 mx-auto"
           >
             <svg className="h-5 w-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
