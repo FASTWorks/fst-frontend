c4a590c1c712d908a2803b35e0d11547b27e54d6
BajiLinux <mr.baji.smart@gmail.com>
2026-05-23 10:52:27 +0700

    Nambahin routing PemasukanPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from "react";
-import { DashboardPage, LoginPage, RegisterPage, NotFound, HomePage } from "./pages/index.js";
+import { DashboardPage, LoginPage, RegisterPage, NotFound, HomePage, PemasukanPage } from "./pages/index.js";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
@@ -9,6 +9,7 @@ const App = () => {
       <Route path="/login" element={<LoginPage/>}/>
       <Route path="/register" element={<RegisterPage/>}/>
       <Route path="/dashboard" element={<DashboardPage/>}/>
+      <Route path="/pemasukan" element={<PemasukanPage/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
   );
