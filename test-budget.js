ccc5b450a1e87f035a43d31208915596e8194f9f
BajiLinux <mr.baji.smart@gmail.com>
2026-06-02 14:15:02 +0700

    minifix(env): hapus file yang tidak berguna

--- test-budget.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-const API = 'https://fst-gateway-service-production.up.railway.app';
-// We don't have a token. But wait, I can login!
--- test-transactions.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const txs = await prisma.transaction.findMany({ orderBy: { transactionDate: 'desc' }, take: 10 });
-  console.log(txs);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
